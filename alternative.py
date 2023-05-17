3ed84f8572366b0dfb9d51a1f1001e1fffeab4df
jawauntb <jawaun.brown95@gmail.com>
2023-05-17 19:28:17 +0000

    commenting out so no breakies

--- alternative.py
+++ alternative.py
@@ -1,56 +1,56 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import librosa
-import numpy as np
-from music21 import pitch
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import librosa
+# import numpy as np
+# from music21 import pitch
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+# @app.route('/')
+# def index():
+#     return 'Hello from Flask!'
 
-@app.route('/extract_notes', methods=['POST'])
-def extract_notes():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
+# @app.route('/extract_notes', methods=['POST'])
+# def extract_notes():
+#     if 'file' not in request.files:
+#         return jsonify({"error": "No file part"}), 400
 
-    file = request.files['file']
-    file_content = file.read()
-    print(f"Received file with size: {len(file_content)}")
-    file.seek(0)
+#     file = request.files['file']
+#     file_content = file.read()
+#     print(f"Received file with size: {len(file_content)}")
+#     file.seek(0)
     
-    # Load the audio file using pydub
-    audio = AudioSegment.from_file(file)
-    print(f"File format: {audio.format}")
-    print(f"File channels: {audio.channels}")
-    print(f"File frame rate: {audio.frame_rate}")
-    print(f"File duration (ms): {len(audio)}")
+#     # Load the audio file using pydub
+#     audio = AudioSegment.from_file(file)
+#     print(f"File format: {audio.format}")
+#     print(f"File channels: {audio.channels}")
+#     print(f"File frame rate: {audio.frame_rate}")
+#     print(f"File duration (ms): {len(audio)}")
 
-    # Create a temporary directory and save the file there
-    temp_dir = tempfile.mkdtemp()
-    file_path = os.path.join(temp_dir, secure_filename(file.filename))
-    # Save the file as a WAV file
-    audio.export(file_path, format="wav")
+#     # Create a temporary directory and save the file there
+#     temp_dir = tempfile.mkdtemp()
+#     file_path = os.path.join(temp_dir, secure_filename(file.filename))
+#     # Save the file as a WAV file
+#     audio.export(file_path, format="wav")
 
-    try:
-        # Load the audio file using librosa
-        y, sr = librosa.load(file_path)
+#     try:
+#         # Load the audio file using librosa
+#         y, sr = librosa.load(file_path)
         
-        # Compute the short-time Fourier Transform (stft)
-        D = np.abs(librosa.stft(y))
+#         # Compute the short-time Fourier Transform (stft)
+#         D = np.abs(librosa.stft(y))
 
-        # Compute the pitch (in Hz)
-        pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+#         # Compute the pitch (in Hz)
+#         pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
 
-        # Select out pitches with high amplitudes
-        pitches = pitches[magnitudes > np.median(magnitudes)]
+#         # Select out pitches with high amplitudes
+#         pitches = pitches[magnitudes > np.median(magnitudes)]
         
-        # Convert frequencies in Hz to note names
-        notes = [pitch.Pitch(freq).name for freq in pitches]
-    finally:
-        # Remove the temporary directory and its contents
-        shutil.rmtree(temp_dir)
+#         # Convert frequencies in Hz to note names
+#         notes = [pitch.Pitch(freq).name for freq in pitches]
+#     finally:
+#         # Remove the temporary directory and its contents
+#         shutil.rmtree(temp_dir)
     
-    return jsonify({"notes": notes}), 200
+#     return jsonify({"notes": notes}), 200
